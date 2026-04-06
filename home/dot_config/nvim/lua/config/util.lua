702119de4ef77f66e51b5912db9c25ddeebad7d8
hidetoshing <hidetoshing@gmail.com>
2026-04-07 00:59:52 +0900

    Add GetPath command for yanking current buffer paths

--- home/dot_config/nvim/lua/config/util.lua
+++ home/dot_config/nvim/lua/config/util.lua
@@ -6,6 +6,45 @@ M.options = {
     -- unnamedplus: + register
 }
 
+local function get_current_buffer_path(opts)
+    local current_file = vim.api.nvim_buf_get_name(0)
+    if current_file == '' then
+        return nil, 'Current buffer is not associated with a file'
+    end
+
+    if opts.absolute then
+        return vim.fn.fnamemodify(current_file, ':p')
+    end
+
+    return vim.fn.fnamemodify(current_file, ':.')
+end
+
+M.setup_get_path = function()
+    vim.api.nvim_create_user_command(
+        'GetPath',
+        function(command_opts)
+            local path, err = get_current_buffer_path({
+                absolute = command_opts.args == 'abs',
+            })
+
+            if err ~= nil then
+                vim.notify(err, vim.log.levels.WARN, { title = 'GetPath' })
+                return
+            end
+
+            vim.fn.setreg('"', path)
+            vim.notify('Yanked: ' .. path, vim.log.levels.INFO, { title = 'GetPath' })
+        end,
+        {
+            nargs = '?',
+            complete = function()
+                return { 'abs' }
+            end,
+            desc = 'Yank current buffer path relative to cwd, or absolute with :GetPath abs',
+        }
+    )
+end
+
 M.setup_config_file = function()
     vim.api.nvim_create_user_command(
         'Config',
@@ -24,6 +63,7 @@ M.setup_movekeys = function()
 end
 
 M.setup = function()
+    M.setup_get_path()
     M.setup_config_file()
     M.setup_movekeys()
 end
